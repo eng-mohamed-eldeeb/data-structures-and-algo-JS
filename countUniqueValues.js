9cf076eac36ff943ed45a7a3cbf010f1380db928
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-06-13 14:30:36 +0200

    Add ;

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -6,14 +6,14 @@ function counUniqueValues(arr) {
         return 1;
     }
     var begin =0, next = 1;
-    while (next <= arr.length) {
+    while (next < arr.length) {
         if (arr[begin] != arr[next]) {
             begin++;
             arr[begin] = arr[next];
         }
         next++;
     }
-    return begin;
+    return begin +1;
 }
 
 console.log(counUniqueValues([3,3,2,2,4,4,5,6]))
