a8ae64ffb5db015e4131465e96987a45a8d95c2d
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-06-15 01:38:47 +0200

    Optimize the bubble sort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,10 +1,14 @@
 function bubbleSort(arr) {
+    let noSwap;
     for (let i = arr.length; i >= 0; i--) {
+        noSwap = true;
         for (let j = 0; j <= i -1; j++) {
             if (arr[j] > arr[j+1]) {
                 [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+                noSwap = false;
             }
         }
+        if (noSwap) break;
     }
     return arr
 }
